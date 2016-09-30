c400310227c3e14fce2e331c7b78bf7cc51ba2eb
meister4ever <sasavulsky@gmail.com>
2016-09-29 22:23:15 -0300

    se sube primer test

---
+++ test_solver.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from ejercicios import Solver
+
+class TestSolver(TestCase):
+    def test_negative_discr(self):
+        s = Solver()
+
+        self.assertRaises(Exception, s.demo, 2, 1, 2)
